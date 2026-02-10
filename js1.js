bf5606cd491f0da1ec1a7fae77c78bf44d8838a9
Jacob W <jacobw318@live.com>
2026-02-10 15:49:33 -0500

    Update js1.js

--- js1.js
+++ js1.js
@@ -1,14 +1,14 @@
 let count = 0;
 
 
-document.getElementById("counter").innerText = data;
+document.getElementById("counter").innerText = count;
 
 function tickup() {
 count = count + 1;
-    document.getElementById("counter").innerText = data;
+    document.getElementById("counter").innerText = count;
 }
 
 function tickdown() {
   count = count - 1;
-    document.getElementById("counter").innerText = data;
+    document.getElementById("counter").innerText = count;
 }
