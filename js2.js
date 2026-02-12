94e89edc0cc647b3e93b5126117a8c10bbd7ba34
Jacob W <jacobw318@live.com>
2026-02-12 16:56:16 -0500

    Create js2.js

---
+++ js2.js
@@ -0,0 +1,9 @@
+function showFilter() {
+    document.getElementById("filterContent").style.display = "block";
+    document.getElementById("newContent").style.display = "none";
+}
+
+function showAddNew() {
+    document.getElementById("newContent").style.display = "flex";
+    document.getElementById("filterContent").style.display = "none";
+}
